5fa9f4e74d97fe7fca8b8b73bbea6dec9000f61f
Alexlozano1023 <alexfavio2@gmail.com>
2025-04-14 21:36:36 -0400

    Cleanup: remove test scripts and files, keep only required submission

--- mywork/Lab_7/mywork/Lab_7/step1_init_client.py
+++
@@ -1,5 +0,0 @@
-import boto3
-
-s3 = boto3.client('s3', region_name='us-east-1')
-print("S3 client initialized.")
-
--- mywork/Lab_7/mywork/Lab_7/step2_list_buckets.py
+++
@@ -1,8 +0,0 @@
-import boto3
-
-s3 = boto3.client('s3', region_name='us-east-1')
-response = s3.list_buckets()
-
-for bucket in response['Buckets']:
-    print(bucket['Name'])
-
