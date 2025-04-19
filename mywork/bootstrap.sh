6a23c8bfd5916efe48b3d2a7ff1de03432cd0c95
Alexlozano1023 <alexfavio2@gmail.com>
2025-04-19 01:41:48 -0400

    Update bootstrap.sh

--- mywork/bootstrap.sh
+++ mywork/bootstrap.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
 
-# Update system packages
+
 apt update -y
 apt upgrade -y
 
-# Install selected tools
+
 apt install -y python3 git nginx
 
-# Start and enable nginx service
+
 systemctl start nginx
 systemctl enable nginx
+
 
