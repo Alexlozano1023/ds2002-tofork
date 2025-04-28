a51f39e08b5434ef22aabd5143b7cb6f7803ee36
Alexlozano1023 <alexfavio2@gmail.com>
2025-04-28 16:34:36 -0400

    Update upload_and_presign.sh

--- mywork/Lab_7/upload_and_presign.sh
+++ mywork/Lab_7/upload_and_presign.sh
@@ -5,9 +5,9 @@ FILE=$1
 BUCKET=$2
 EXPIRATION=$3
 
-# Upload file to S3
+
 aws s3 cp "$FILE" s3://$BUCKET/
 
-# Generate a presigned URL
+
 aws s3 presign s3://$BUCKET/$FILE --expires-in $EXPIRATION
 
